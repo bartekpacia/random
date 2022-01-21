d0f0635e603f1c0ab27e25307146bf0ed82c8a7d
Bartek Pacia <barpac02@gmail.com>
2022-01-21 19:15:39 +0100

    insert 2

--- bubble.c
+++ bubble.c
@@ -14,13 +14,14 @@ void bubble_sort(int* array, int length) {
 }
 
 void insert_sort(int* array, int length) {
-    int i, j;
-    for (i = 1; i < length; ++i) {
-        int value = array[i];
-        for (j = i - 1; j >= 0 && array[j] > array[i]; --j) {
+    for (int i = 1; i < length; i++) {
+        int key = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > key) {
             array[j + 1] = array[j];
+            j = j - 1;
         }
-        array[j + 1] = value;
+        array[j + 1] = key;
     }
 }
 
