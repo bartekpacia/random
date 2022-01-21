fbe58437b572063a13a518cf93efe1482f152500
Bartek Pacia <barpac02@gmail.com>
2022-01-21 19:06:44 +0100

    bubble

---
+++ bubble.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/// Sortuje tablicę "array" o długości "length" metodą bąbelkową.
+void bubble_sort(int* array, int length) {
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = 0; j < length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int length = 9;
+    int arr[] = {7, 4, 2, 6, 3, 1, 8, 9, 5};
+    bubble_sort(arr, length);
+
+    for (int i = 0; i < length; i++) {
+        printf("%i ", arr[i]);
+    }
+
+    printf("\n");
+}
