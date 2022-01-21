fbf1c65416a218aea3ba9a0c54b12d93c2fe70a6
Bartek Pacia <barpac02@gmail.com>
2022-01-21 19:30:27 +0100

    add bubble

--- bubble.c
+++ bubble.c
@@ -13,6 +13,7 @@ void bubble_sort(int* array, int length) {
     }
 }
 
+/// Sortuje tablicę "array" o długości "length" metodą przez proste wstawianie.
 void insert_sort(int* array, int length) {
     for (int i = 1; i < length; i++) {
         int key = array[i];
@@ -25,10 +26,39 @@ void insert_sort(int* array, int length) {
     }
 }
 
+void swap(int* first, int* second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+int partition(int* arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/// Sortuje tablicę "array" o długości "length" metodą szybkiego
+void quick_sort(int* arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
 int main() {
     int length = 9;
     int arr[] = {7, 4, 2, 6, 3, 1, 8, 9, 5};
-    insert_sort(arr, length);
+    quick_sort(arr, 0, length);
 
     for (int i = 0; i < length; i++) {
         printf("%i ", arr[i]);
