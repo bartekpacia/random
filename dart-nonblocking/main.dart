223ad0f76bfe000bc683b5529a6b3386e264c51d
Bartek Pacia <barpac02@gmail.com>
2024-03-11 21:54:41 +0000

    experiment with non-blocking Dart CLI: more variants to test

--- dart-nonblocking/main.dart
+++ dart-nonblocking/main.dart
@@ -1,30 +1,36 @@
-import 'dart:async';
 import 'dart:isolate';
 import 'dart:io' as io;
 
 void main(List<String> args) {
-	final sw = Stopwatch()..start();
-	final String arg = args.first;
+  final sw = Stopwatch()..start();
+  final String arg = args.first;
 
-	if (arg == "future") {
-		final sw = Stopwatch()..start();
-		unawaited(_longWork());
-		print("started long work");
-	} else if (arg == "isolate") {
-		// unawaited(Isolate.run(_longWork));
-    Isolate.spawn((message) {
-      _longWork();
-    }, null);
-		print("started long work in isolate");
-	} else {
-		print('pas either "future" or "isolate" as an argument');
-		io.exit(1);
-	}
+  if (arg == "future") {
+    final sw = Stopwatch()..start();
+    _longWork();
+    print("started long work: future");
+  } else if (arg == "isolate_run") {
+    Isolate.run(_longWork);
+    print("started long work: isolate_run");
+  } else if (arg == "isolate_spawn") {
+    Isolate.spawn((message) => _longWork(), null);
+    print("started long work: isolate_spawn_closure");
+  } else if (arg == "isolate_spawn_closure") {
+    final int closedOver = 2137;
+    final void Function(int?) closure = (int? _) {
+      print('I captured int: $closedOver');
+    };
+    Isolate.spawn(closure, null);
+    print("started long work: isolate_spawn_closure");
+  } else {
+    print('error: unknown argument');
+    io.exit(1);
+  }
 
-	final time = sw.elapsed.inMilliseconds;
-	print('Total time: $time');
+  final time = sw.elapsed.inMilliseconds;
+  print('Total time: $time');
 }
 
-Future <void> _longWork() async {
-	await Future.delayed(const Duration(seconds: 3));
+Future<void> _longWork() async {
+  await Future.delayed(const Duration(seconds: 3));
 }
