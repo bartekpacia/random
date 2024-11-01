bdc28df7a88afdb08f9c865b11021186be763c2d
Bartek Pacia <barpac02@gmail.com>
2024-11-01 18:59:28 +0100

    better demo

--- java/examplelib/src/main/java/pacia/tech/dummy/DummyLib.java
+++ java/examplelib/src/main/java/pacia/tech/dummy/DummyLib.java
@@ -2,9 +2,15 @@ package pacia.tech.dummy;
 
 import org.jetbrains.annotations.NotNull;
 
+/// Showcases differences between 3 decompiler presets:
+/// 1. Diff high-Medium: default constructor visibility
+/// 2. Diff medium-low: decompile assertions, decompile intellij annotation, decompile enums
 public class DummyLib {
+    private static int a = 1;
+
     public static void main(String[] args) {
         String result = foo(null);
+        assert 1 + a == 2;
         System.out.println("hello, comparison result: " + result);
     }
 
@@ -12,6 +18,4 @@ public class DummyLib {
         boolean result = bar.equals("baz");
         return "blah " + bar;
     }
-
-    public enum ExampleNestedEnum {RED, GREEN, BLUE}
 }
