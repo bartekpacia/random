4f023f91bdb17b310b80fa9dc29db3a1c722d0fb
Bartek Pacia <barpac02@gmail.com>
2024-11-02 03:36:14 +0100

    go/go-cobra: investigating most common shell completion scenarios

--- go/go-cobra/README.md
+++ go/go-cobra/README.md
@@ -28,4 +28,64 @@ Test flag completion:
 Barteks-iPhone  emulator-5554
 ```
 
+# Shell completion – feature overview
+
+To test out various shell completion features, we'll build out a hypothetical simple app for controlling Android
+emulators and iOS simulators.
+
+## Complete commands and options
+
+```console
+./go-cobra [tab]
+```
+
+## Complete single element
+
+Use case: when an option has been selected, don't suggest any more completions.
+
+t=1
+
+```console
+./go-cobra run [tab]
+Pixel 7 API 34
+iPhone 12 mini
+iPhone 15
+```
+
+t=2
+
+```console
+./go-cobra run Pixel\ 7\ API\ 34 [tab] # nothing appears, selection has been made
+```
+
+The default Zsh behavior is dependent upon some `zstyle` options. By default, it suggestings files in $pwd.
+
+## Complete finite set of elements
+
+Use case: when a single option is selected, keep suggesting options but exclude the options already
+completed/entered/selected.
+
+t=1
+
+```console
+./go-cobra run [tab]
+Pixel 7 API 34
+iPhone 12 mini
+iPhone 15
+```
+
+t=1
+
+```console
+./go-cobra run Pixel 7 API 34 [tab]
+iPhone 12 mini
+iPhone 15
+```
+
+t=2
+
+```console
+./go-cobra run Pixel 7 API 34 iPhone 12 mini
+```
+
 [docs]: https://github.com/spf13/cobra/blob/v1.8.0/site/content/completions/_index.md
--- go/go-cobra/main.go
+++ go/go-cobra/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"slices"
@@ -8,27 +9,32 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func main() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
-}
+var devices = []string{"Pixel 7 API 34", "iPhone 12 mini", "iPhone 15"}
 
-var rootCmd = &cobra.Command{
-	Use:               "go-cobra",
-	Short:             "A brief description of your application",
-	ValidArgsFunction: cobra.NoFileCompletions,
-}
+var killDevice string
 
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "Run app on device",
+	Short: "Start a single device",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		fmt.Printf("run called with args: %#v\n", args)
 	},
 	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		completions := []string{"app1", "app2", "app3"}
+		// Use "cobra.ShellCompDirectiveDefault" to see files being suggested. EDIT: Not true.
+		// Use "cobra.ShellCompDirectiveNoFileComp" to stop suggestions after completion is accepted.
+		return devices, cobra.ShellCompDirectiveDefault
+	},
+}
+
+var runAllCmd = &cobra.Command{
+	Use:   "runall",
+	Short: "Start many devices",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("run called with args: %#v\n", args)
+	},
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		completions := make([]string, len(devices))
+		copy(completions, devices)
 		for _, arg := range args {
 			for i, completion := range completions {
 				if arg == completion {
@@ -41,17 +47,61 @@ var runCmd = &cobra.Command{
 	},
 }
 
+// This command doesn't accept the device to kill through argument, but through option/flag
+var killCmd = &cobra.Command{
+	Use:   "kill",
+	Short: "Kill a single device",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if killDevice == "" {
+			return errors.New("required flag 'device' not set")
+		}
+		fmt.Printf("kill called with value: %#v\n", killDevice)
+		return nil
+	},
+	//ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	//	completions := []string{"app1", "app2", "app3"}
+	//	for _, arg := range args {
+	//		for i, completion := range completions {
+	//			if arg == completion {
+	//				completions = slices.Delete(completions, i, i+1)
+	//			}
+	//		}
+	//	}
+	//
+	//	// Use "cobra.ShellCompDirectiveDefault" to see files being suggested.
+	//	return completions, cobra.ShellCompDirectiveNoFileComp
+	//},
+}
+
 func init() {
 	rootCmd.AddCommand(runCmd)
+	rootCmd.CompletionOptions.HiddenDefaultCmd = true
 
-	runCmd.Flags().String("device", "", "Select device to run on")
+	rootCmd.AddCommand(runAllCmd)
 
-	runCmd.RegisterFlagCompletionFunc(
+	rootCmd.AddCommand(killCmd)
+	killCmd.Flags().StringVar(&killDevice, "device", "", "The device to kill")
+	_ = killCmd.MarkFlagRequired("device")
+	_ = killCmd.RegisterFlagCompletionFunc(
 		"device",
 		func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			cobra.CompDebug(fmt.Sprintf("args: %v, toComplete: %s\n", args, toComplete), false)
 
-			return []string{"emulator-5554", "Barteks-iPhone"}, cobra.ShellCompDirectiveNoFileComp
+			// Use "cobra.ShellCompDirectiveDefault" to see files being suggested.
+			return devices, cobra.ShellCompDirectiveNoFileComp
 		},
 	)
+}
+
+func main() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:               "go-cobra",
+	Short:             "A brief description of your application",
+	ValidArgsFunction: cobra.NoFileCompletions,
 }
