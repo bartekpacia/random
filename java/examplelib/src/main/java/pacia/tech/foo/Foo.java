2c380a945c89bd2c83eb235d260de4c037b0f1a7
Bartek Pacia <barpac02@gmail.com>
2024-11-04 12:05:13 +0100

    create Foo example for decompiler

---
+++ java/examplelib/src/main/java/pacia/tech/foo/Foo.java
@@ -0,0 +1,9 @@
+package pacia.tech.foo;
+
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public enum ExampleNestedEnum {RED, GREEN, BLUE}
+}
