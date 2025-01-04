f92cdc9582c1e26cf71ac226008a3305a397514d
Bartek Pacia <barpac02@gmail.com>
2025-01-04 21:28:50 +0100

    run!

---
+++ pi-pico/data_transfer.py
@@ -0,0 +1,12 @@
+import time
+import sys
+
+while True:
+    # read a command from the host
+    v = sys.stdin.readline().strip()
+    print(f"received '{v}'")
+    time.sleep(1)
+    
+    sys.stdout.print(b"pong\n")
+    #  sys.stdout.write(b"pong\n")
+    print("sent 'pong'")
