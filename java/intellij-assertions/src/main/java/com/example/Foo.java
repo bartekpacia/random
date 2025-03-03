f6da64f0547a755877e30fccb6e46160be04f877
Bartek Pacia <barpac02@gmail.com>
2025-03-03 16:56:48 +0100

    repro IDEA-368266

--- java/intellij-assertions/src/main/java/com/example/Foo.java
+++ java/intellij-assertions/src/main/java/com/example/Foo.java
@@ -1,15 +1,38 @@
 package com.example;
 
 public class Foo {
-    private final String data;
+  private final String data;
 
-    Foo(String data) {
-        this.data = data;
+  Foo(String data) {
+    this.data = data;
+  }
+
+  public boolean isValid() {
+    return data.length() % 2 == 0;
+  }
+
+  // some other methods that make use of the encapsulated information, for example returning it in different formats
+
+  private String blah(int foo) {
+    String path1;
+    String path2;
+
+    switch (foo) {
+      case 10:
+        return "here";
+      case 11:
+        return "case";
     }
 
-    public boolean isValid() {
-        return data.length() % 2 == 0;
+    switch (foo) {
+      case 1 -> path1 = "bar";
+      case 2 -> path2 = "Hello";
     }
 
-    // some other methods that make use of the encapsulated information, for example returning it in different formats
+    return switch (foo) {
+      case 1 -> "here";
+      case 2 -> "Hello";
+      default -> "idk";
+    };
+  }
 }
