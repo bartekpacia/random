4bf14da4d0d31b51a7f0fab776eae6cdbe99f3d0
Bartek Pacia <barpac02@gmail.com>
2025-03-13 16:06:03 +0100

    records

---
+++ java/intellij-assertions/src/main/java/com/example/BlahClasses.java
@@ -0,0 +1,112 @@
+package com.example;
+
+public class BlahClasses {
+  private int instanceInt = 1;
+  private static int staticInt = 1;
+
+  public class Foo {
+    void foo() {
+      var a = instanceInt;
+    }
+  }
+
+  private Listener listener = new Listener() {
+    @Override
+    public void onClick() {
+
+    }
+  };
+
+  public void main() {
+    int localVar = 2;
+
+    class PossiblyStaticLocalClass {
+      private final String name;
+
+      PossiblyStaticLocalClass(String name) {
+        this.name = name;
+      }
+
+      public String getName() {
+        return name;
+      }
+
+      public int nested() {
+        return staticInt;
+      }
+    }
+
+    class ImpossiblyStaticLocalClass_1 {
+      private final String name;
+
+      ImpossiblyStaticLocalClass_1(String name) {
+        this.name = name;
+      }
+
+      public String getName() {
+        return name;
+      }
+
+      public int nested() {
+        return instanceInt;
+      }
+    }
+
+    class ImpossiblyStaticLocalClass_2 {
+      private final String name;
+
+      ImpossiblyStaticLocalClass_2(String name) {
+        this.name = name;
+      }
+
+      public String getName() {
+        return name;
+      }
+
+      public int nested() {
+        return localVar;
+      }
+    }
+
+    record Record(String name) {
+      int foo() {
+        // return a;
+        return staticInt;
+      }
+    }
+  }
+}
+
+interface Listener {
+  void onClick();
+}
+
+
+class Bar {
+  void test() {
+    int localVar = 2;
+
+    class Point {
+      private final double x;
+      private final double y;
+
+      Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+      }
+
+      public double getX() {
+        return x;
+      }
+
+      public double getY() {
+        return y;
+      }
+
+      Point shiftX() {
+        double a = x;
+        return new Point(x + 1, y);
+      }
+    }
+  }
+}
--- java/intellij-assertions/src/main/java/com/example/ReplaceConstructorWithFactoryAction.java
+++ java/intellij-assertions/src/main/java/com/example/ReplaceConstructorWithFactoryAction.java
@@ -1,5 +1,27 @@
 package com.example;
 
-public class ReplaceConstructorWithFactoryAction {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+final class ReplaceConstructorWithFactoryAction {
   void a() {
 
+
+  }
+}
+
+public final class ErrorProneModelSerializerExtension extends JpsModelSerializerExtension {
+
+  private ErrorProneModelSerializerExtension() {
+  }
+
+  public static ErrorProneModelSerializerExtension createErrorProneModelSerializerExtension() {
+    return new ErrorProneModelSerializerExtension();
+  }
+
+  @Override
+  public @NotNull List<? extends JpsProjectExtensionSerializer> getProjectExtensionSerializers() {
+    return null;
+  }
+}
