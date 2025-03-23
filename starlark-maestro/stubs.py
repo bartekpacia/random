ae8dd6f2fc16f38f2a4f277abb5dc849a34b1e6e
Bartek Pacia <barpac02@gmail.com>
2025-03-23 22:07:35 +0100

    aaaa

---
+++ starlark-maestro/stubs.py
@@ -0,0 +1,11 @@
+# stubs for use with github.com/tilt-dev/starlark-lsp
+
+def foo(name: str) -> bool:
+  """
+  This is sample documentation of the foo function.
+  """
+  pass
+
+def launch_app(app_id, clear_state, permissions) -> None:
+  pass
+
