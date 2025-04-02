d279cbdb458ca8a6fe9d332e3a79e1769b3ae86e
Bartek Pacia <barpac02@gmail.com>
2025-04-02 11:20:39 +0200

    samples

--- java/demo_lombok/src/main/java/org/example/Main.java
+++ java/demo_lombok/src/main/java/org/example/Main.java
@@ -6,18 +6,30 @@ import java.util.List;
 
 @Value
 class User {
-    String name;
-    int age;
+    private String name;
+    private int age;
+
     static int lol;
 }
+
+@Value
+class Person {
+    private String name; // redundant because (1)
+    final int age; // redundant because (2)
+    private final String city; // redundant because (1) and (2)
+}
+
 
 public class Main {
     public static void main(String[] args) {
         List<String> a;
         User user = new User("Louis", 27);
+        Person person = new Person("Bart", 29, "Utrecht");
         System.out.printf("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
+        System.out.println("person " + person.getName() + " lives in " + person.getCity());
     }
 }
