d58c77f97470c74e926759e77dee91ce81a2ac69
Bartek Pacia <barpac02@gmail.com>
2025-04-02 11:14:24 +0200

    aaaa

--- java/demo_lombok/src/main/java/org/example/Main.java
+++ java/demo_lombok/src/main/java/org/example/Main.java
@@ -1,20 +1,20 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Value;
 
-@AllArgsConstructor
-@Getter
-@ToString
+import java.util.List;
+
+@Value
 class User {
-    private String name;
-    private int age;
+    String name;
+    int age;
+    static int lol;
 }
 
 public class Main {
     public static void main(String[] args) {
+        List<String> a;
+        User user = new User("Louis", 27);
         System.out.printf("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
