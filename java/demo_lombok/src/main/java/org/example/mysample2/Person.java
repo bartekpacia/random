b18ba615d2449d84dfc2f92e55a14ecb6a9bc8be
Bartek Pacia <barpac02@gmail.com>
2025-04-24 11:49:51 +0200

    asd

--- java/demo_lombok/src/main/java/org/example/mysample2/Person.java
+++ java/demo_lombok/src/main/java/org/example/mysample2/Person.java
@@ -2,24 +2,90 @@ package org.example.mysample2;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.Value;
 import lombok.experimental.FieldDefaults;
 
-@Getter
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode
-class Person {
-    final String name;
-    final int age;
+import java.util.Objects;
+
+public class Person {
+    public static void main(String[] args) {
+        Person3 person3 = new Person3("John");
+        System.out.println("person3 = " + person3);
+    }
 }
 
-@Value
+@AllArgsConstructor
+class Person1 {
+    final String name;
+    final int age;
+
+    public Person1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+@ToString
+@EqualsAndHashCode
 class Person2 {
     String name;
     int age;
+
+    @Override
+    public String toString() {
+        return "Person2{name='" + name + "', age=" + age + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person2 person2 = (Person2) o;
+        return age == person2.age && Objects.equals(name, person2.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
+
+@ToString
+class Person3 {
+    final String name;
+    final int age;
+
+    public Person3(String name) {
+        this.name = name;
+        this.age = 42;
+    }
+
+    public Person3(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + '}';
+    }
+}
+
+@AllArgsConstructor
+record Person4(String name, int age) {
+}
+
+@Value
+record Person5(String name, int age) {
 }
