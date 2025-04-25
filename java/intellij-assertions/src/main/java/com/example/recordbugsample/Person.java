8bc691e273086b89ef2bb1737fd62f8600e84763
Bartek Pacia <barpac02@gmail.com>
2025-04-25 16:13:44 +0200

    cbkahasdas

---
+++ java/intellij-assertions/src/main/java/com/example/recordbugsample/Person.java
@@ -0,0 +1,14 @@
+package com.example.recordbugsample;
+
+class Person {
+  final String name;
+  final int age;
+
+  Person(String myName, int myAge) {
+    this.name = myName;
+    this.age = myAge;
+  }
+}
+
+
+
