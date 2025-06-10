0319798879481a6e97e08d2ea5ec28a704a76356
Bartek Pacia <barpac02@gmail.com>
2025-06-10 10:54:53 +0200

    progres

--- java/demo_cucumber/src/test/java/atm/WithdrawSteps.java
+++ java/demo_cucumber/src/test/java/atm/WithdrawSteps.java
@@ -1,5 +1,6 @@
 package atm;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,30 +14,38 @@ public class WithdrawSteps {
     }
 
     @When("^I withdraw (-?\\d+) EUR$")
-    public void withdraw_EUR(int EUR) throws Throwable {
+    public void withdraw_EUR(int EUR) {
     }
 
     @Then("I get {int} EUR from the ATM")
-    public void i_get_EUR_from_the_ATM(int EUR) throws Throwable {
+    public void i_get_EUR_from_the_ATM(int EUR) {
     }
 
     @Then("error message about the lack of money is displayed")
-    public void error_message_about_the_lack_of_money_is_displayed() throws Throwable {
+    public void error_message_about_the_lack_of_money_is_displayed() {
     }
 
     @Then("My account has {int} EUR left")
-    public void my_account_has_EUR_left(int EUR) throws Throwable {
+    public void my_account_has_EUR_left(int EUR) {
     }
 
     @Then("error message about incorrect amount is displayed")
-    public void error_message_about_incorrect_amount_is_displayed() throws Throwable {
+    public void error_message_about_incorrect_amount_is_displayed() {
     }
 
     @Then("I am happy")
-    public void i_am_happy() throws Throwable {
+    public void i_am_happy() {
     }
 
     @Then("^I am angry$")
-    public void i_am_angry() throws Throwable {
+    public void i_am_angry() {
+    }
+
+    @Given("I don't have any euro(s) on my account")
+    public void I_dont_have_any_euros(int euros) {
+    }
+
+    @Then("I have single/many emotion(s)")
+    public void I_have_emotions() {
     }
 }
--- java/demo_cucumber/src/test/resources/atm/withdraw.feature
+++ java/demo_cucumber/src/test/resources/atm/withdraw.feature
@@ -33,3 +33,16 @@ Feature: Withdraw money
     And My account has 50 EUR left
     And error message about incorrect amount is displayed
     And I am angry
+
+  Scenario: Lack of money causes emotional breakdown
+    Given I don't have any euros on my account
+    When I withdraw 1 EUR
+    Then error message about the lack of money is displayed
+    And I have single/many emotions
+
+# 1. Expected: I have {single/many} emotions -> TAB
+# 2. Expected: I have |
+#                      single
+#                      many
+
+# Do it with a template. Or maybe ModCommands?
