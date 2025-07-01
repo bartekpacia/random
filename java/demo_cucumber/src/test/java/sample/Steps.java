2d124ef4e08f1d459696e0b94711d1a6ef3da00c
Bartek Pacia <barpac02@gmail.com>
2025-07-01 18:25:38 +0200

    adadsasda

--- java/demo_cucumber/src/test/java/sample/Steps.java
+++ java/demo_cucumber/src/test/java/sample/Steps.java
@@ -1,15 +1,9 @@
 package sample;
 
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 
 public class Steps {
     @Then("I have {int} cucumber(s) in my belly")
     public void I_have_cucumbers_in_my_belly(int cukes) {
-
-    }
-
-    @When("I eat {int} cucumber(s)")
-    public void iEatCucumber(int cukes) {
     }
 }
