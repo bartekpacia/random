499a38edf70e88fa3a349480279b1517935d507b
Bartek Pacia <barpac02@gmail.com>
2025-07-07 16:42:48 +0200

    hihi

---
+++ java/simple/src/main/java/records25/Person.java
@@ -0,0 +1,11 @@
+package records25;
+
+public class Person {
+    final String name;
+    final String age;
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+}
--- java/simple/src/main/java/simple2/Main.java
+++ java/simple/src/main/java/simple2/Main.java
@@ -1,12 +1,23 @@
 package simple2;
 
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+// problems: (1) not possible to move tabs  (2) respect file name extension setting
+// TODO: ask #ij-ui
 public class Main {
     public static String name = "Charlie";
     public static Object obj = new Object();
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        int argCount = args.length;
-        System.out.println("there are " + argCount + " args");
+        Semaphore semaphore = new Semaphore(1);
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+
+        List<String> list = List.of();
+        list.stream().map((a) -> {
+            return a.toLowerCase();
+        });
     }
 }
