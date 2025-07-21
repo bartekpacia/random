91558132c75fc8f534afe6cf3fa15fa4f3e16cb1
Bartek Pacia <barpac02@gmail.com>
2025-07-21 13:42:20 +0200

    regex ddos

--- java/simple/src/main/java/simple1/Main.java
+++ java/simple/src/main/java/simple1/Main.java
@@ -1,7 +1,24 @@
 package simple1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// demo of regex DDOS
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        for (int i = 10; i < 1_000_000_000; i = i * 10) {
+            System.out.println("-> TEST for len(str) = " + i);
+
+            String text = "a".repeat(i);
+            Pattern pattern = Pattern.compile("(a*)*b");
+
+            long startTime = System.nanoTime();
+            Matcher matcher = pattern.matcher(text);
+
+            matcher.find();
+
+            long endTime = System.nanoTime();
+            System.out.println("Pattern matcher execution time: " + ((endTime - startTime) / 1_000_000) + " ms");
+        }
     }
 }
