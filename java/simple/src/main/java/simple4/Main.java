6c7e67940595a4e617b5fb99e1aa58ef443fb78a
Bartek Pacia <barpac02@gmail.com>
2025-10-01 17:18:59 +0200

    okadasd

--- java/simple/src/main/java/simple4/Main.java
+++ java/simple/src/main/java/simple4/Main.java
@@ -3,10 +3,5 @@ package simple4;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-
-        Thread t = new Thread(() -> {
-
-        });
-        t.start();
     }
 }
