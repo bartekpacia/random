0d56776ad73a2f677a9ce83381a1c4358c567b28
Bartek Pacia <barpac02@gmail.com>
2025-10-20 14:50:53 +0200

    woo cond bool expr

--- java/simple/src/main/java/simple1/Main.java
+++ java/simple/src/main/java/simple1/Main.java
@@ -1,5 +1,10 @@
 package simple1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
--- java/simple/src/main/java/simple4/Main.java
+++ java/simple/src/main/java/simple4/Main.java
@@ -1,7 +1,25 @@
 package simple4;
 
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+    }
+}
+
+class BooleanStringTernaryParen {
+    void foo(boolean param) {
+        String output1 = param ? "true" : "false";
+        String output2 = param ? "false" : "true";
+    }
+
+    void foo(Boolean param) {
+        String output1 = param ? ("true") : "false";
+        String output2 = ((param)) ? ("true") : "false";
+    }
+
+    public String foo2(Boolean value) {
+        return value ? "true" : "false";
     }
 }
