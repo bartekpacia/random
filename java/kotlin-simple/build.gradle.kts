33a66a02183d772422d526e6c27fb267e0a10761
Bartek Pacia <barpac02@gmail.com>
2025-11-17 15:41:36 +0100

    create kotlin-simple

---
+++ java/kotlin-simple/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "tech.pacia"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
