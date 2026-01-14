bfaeff503a95cf802cc5ebe6f86e010dd0a72bcc
Bartek Pacia <barpac02@gmail.com>
2026-01-15 00:11:21 +0100

    indexer: extract features into functions

--- indexer/src/main/java/tech/pacia/indexer/Main.java
+++ indexer/src/main/java/tech/pacia/indexer/Main.java
@@ -26,74 +26,11 @@ public class Main {
         String searchWord = args[3].toLowerCase(Locale.ROOT);
         switch (command) {
             case "search-dumb": {
-
-                int lineNumber = 1;
-                try (BufferedReader reader = Files.newBufferedReader(path)) {
-                    String line;
-                    int occurrence = 1;
-                    while ((line = reader.readLine()) != null) {
-                        int offset = 0;
-                        while ((offset = line.toLowerCase(Locale.ROOT).indexOf(searchWord, offset)) != -1) {
-                            offset++; // let's start from 1, not from 0
-                            Position position = new Position(path, lineNumber, offset);
-                            System.out.println(position);
-                            //System.out.println("found occurrence " + occurrence + " in " + path + "@" + lineNumber + ":" + offset);
-                            occurrence++;
-                            offset++;
-                        }
-
-                        lineNumber++;
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException("failed to read from " + path, e);
-                }
+                dumbSearch(path, searchWord);
                 break;
             }
             case "build-index": {
-                Map<String, List<Position>> index = new HashMap<>();
-                Set<Character> ignored = Set.of(' ', '.', ',', ';', '“', '”', '\n');
-                int lineNumber = 0;
-                long startMillis = System.currentTimeMillis();
-                try (BufferedReader reader = Files.newBufferedReader(path)) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        int i = 0;
-                        int n = line.length();
-                        while (i < n) {
-                            while (i < n && ignored.contains(line.charAt(i))) {
-                                i++;
-                            }
-                            int start = i;
-                            while (i < n && !ignored.contains(line.charAt(i))) {
-                                i++;
-                            }
-                            if (start < i) {
-                                String key = line.substring(start, i).toLowerCase();
-                                Position position = new Position(path, lineNumber, start);
-                                index.computeIfAbsent(key, (k) -> new ArrayList<>()).add(position);
-                            }
-                        }
-
-                        lineNumber++;
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException("failed to read from " + path, e);
-                }
-
-                long duration = System.currentTimeMillis() - startMillis;
-                System.out.println("built index (" + index.size() + " keys) in " + duration + " ms");
-
-                List<Position> positions = index.get(searchWord);
-                if (positions == null) {
-                    System.out.println("no entry in index for key " + searchWord);
-                    System.exit(0);
-                }
-                // System.out.println("result: " + positions);
-                for (int i = 0; i < positions.size(); i++) {
-                    Position position = positions.get(i);
-                    System.out.println(position);
-                }
-
+                buildIndex(path, searchWord);
                 break;
             }
             case "search-index": {
@@ -106,7 +43,77 @@ public class Main {
         }
 
     }
+
+    private static void dumbSearch(Path path, String searchWord) {
+        int lineNumber = 1;
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            int occurrence = 1;
+            while ((line = reader.readLine()) != null) {
+                int offset = 0;
+                while ((offset = line.toLowerCase(Locale.ROOT).indexOf(searchWord, offset)) != -1) {
+                    offset++; // let's start from 1, not from 0
+                    Position position = new Position(path, lineNumber, offset);
+                    System.out.println(position);
+                    //System.out.println("found occurrence " + occurrence + " in " + path + "@" + lineNumber + ":" + offset);
+                    occurrence++;
+                    offset++;
+                }
+
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("failed to read from " + path, e);
+        }
+    }
+
+    private static void buildIndex(Path path, String searchWord) {
+        Map<String, List<Position>> index = new HashMap<>();
+        Set<Character> ignored = Set.of(' ', '.', ',', ';', '“', '”', '\n');
+        int lineNumber = 0;
+        long startMillis = System.currentTimeMillis();
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int i = 0;
+                int n = line.length();
+                while (i < n) {
+                    while (i < n && ignored.contains(line.charAt(i))) {
+                        i++;
+                    }
+                    int start = i;
+                    while (i < n && !ignored.contains(line.charAt(i))) {
+                        i++;
+                    }
+                    if (start < i) {
+                        String key = line.substring(start, i).toLowerCase();
+                        Position position = new Position(path, lineNumber, start);
+                        index.computeIfAbsent(key, (k) -> new ArrayList<>()).add(position);
+                    }
+                }
+
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("failed to read from " + path, e);
+        }
+
+        long duration = System.currentTimeMillis() - startMillis;
+        System.out.println("built index (" + index.size() + " keys) in " + duration + " ms");
+
+        List<Position> positions = index.get(searchWord);
+        if (positions == null) {
+            System.out.println("no entry in index for key " + searchWord);
+            System.exit(0);
+        }
+        // System.out.println("result: " + positions);
+        for (int i = 0; i < positions.size(); i++) {
+            Position position = positions.get(i);
+            System.out.println(position);
+        }
+    }
 }
+
 
 record Position(Path path, int line, int offset) {
     @Override
