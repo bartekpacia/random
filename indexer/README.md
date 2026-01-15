9d89efd2ccc0b9463c1b0a079dcd2361829d8306
Bartek Pacia <barpac02@gmail.com>
2026-01-15 01:08:44 +0100

    indexer: add Unix Domain Socket support for indexed search

--- indexer/README.md
+++ indexer/README.md
@@ -10,12 +10,22 @@ Fetch the sample data:
 ./get_data
 ```
 
+Run a simple search without using an index:
+
 ```sh
 ./indexer search-dumb ./data/the_holow_men.txt "we"
 ```
 
+To use an index, first start the indexer:
+
 ```sh
-./indexer build-index ./data/pride_and_prejudice.txt "we"
+./indexer build-index ./data/pride_and_prejudice.txt
+```
+
+Then connect to the indexer over a Unix Domain Socket and query it:
+
+```
+./indexer query "world"
 ```
 
 ### Resources
--- indexer/src/main/java/tech/pacia/indexer/Main.java
+++ indexer/src/main/java/tech/pacia/indexer/Main.java
@@ -2,13 +2,22 @@ package tech.pacia.indexer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    /// Will be `/tmp/indexer.sock` on Unix/macOS.
+    static final Path socketPath = Path.of(System.getProperty("java.io.tmpdir"), "indexer.sock");
+
+    static void main(String[] args) throws IOException {
         if (args.length < 2) {
             System.err.println("invalid arguments. Usage: ./indexer <command> <file> <word>");
             System.exit(1);
@@ -16,24 +25,33 @@ public class Main {
 
         String command = args[0];
 
-        Path path = Paths.get(args[1]);
-        if (!Files.exists(path)) {
-            System.err.println("file does not exist: " + path);
-            System.exit(1);
-        }
-
         // commands = search-dumb / build-index / search-index
-        String searchWord = args[3].toLowerCase(Locale.ROOT);
         switch (command) {
             case "search-dumb": {
+                Path path = Paths.get(args[1]);
+                if (!Files.exists(path)) {
+                    System.err.println("file does not exist: " + path);
+                    System.exit(1);
+                }
+
+                String searchWord = args[3].toLowerCase(Locale.ROOT);
                 dumbSearch(path, searchWord);
                 break;
             }
             case "build-index": {
-                buildIndex(path, searchWord);
+                Path path = Paths.get(args[1]);
+                if (!Files.exists(path)) {
+                    System.err.println("file does not exist: " + path);
+                    System.exit(1);
+                }
+
+                Map<String, List<Position>> index = buildIndex(path);
+                startServer(index);
                 break;
             }
             case "search-index": {
+                String searchWord = args[1].toLowerCase(Locale.ROOT);
+                query(searchWord);
                 break;
             }
             default: {
@@ -67,7 +85,7 @@ public class Main {
         }
     }
 
-    private static void buildIndex(Path path, String searchWord) {
+    private static Map<String, List<Position>> buildIndex(Path path) throws IOException {
         Map<String, List<Position>> index = new HashMap<>();
         Set<Character> ignored = Set.of(' ', '.', ',', ';', '“', '”', '\n');
         int lineNumber = 0;
@@ -94,22 +112,80 @@ public class Main {
 
                 lineNumber++;
             }
-        } catch (IOException e) {
-            throw new RuntimeException("failed to read from " + path, e);
         }
 
         long duration = System.currentTimeMillis() - startMillis;
         System.out.println("built index (" + index.size() + " keys) in " + duration + " ms");
 
-        List<Position> positions = index.get(searchWord);
-        if (positions == null) {
-            System.out.println("no entry in index for key " + searchWord);
-            System.exit(0);
+        return index;
+    }
+
+    private static void startServer(Map<String, List<Position>> index) throws IOException {
+        // Delete stale socket file if exists
+        Files.deleteIfExists(socketPath);
+
+        UnixDomainSocketAddress address = UnixDomainSocketAddress.of(socketPath);
+        try (ServerSocketChannel server = ServerSocketChannel.open(StandardProtocolFamily.UNIX)) {
+            server.bind(address);
+            System.out.println("server bound to " + address);
+
+            while (true) {
+                SocketChannel client = server.accept();
+                // Read query, search index, write results
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                StringBuilder sb = new StringBuilder();
+                while (client.read(buffer) > 0) {
+                    buffer.flip();
+                    sb.append(StandardCharsets.UTF_8.decode(buffer));
+                    buffer.clear();
+                    if (sb.toString().contains("\n")) {
+                        break;
+                    }
+                }
+                String searchWord = sb.toString().toLowerCase(Locale.ROOT).trim();
+                System.out.println("--> got query: " + searchWord);
+
+                List<Position> positions = index.get(searchWord);
+                if (positions == null) {
+                    System.out.println("no entry in index for key " + searchWord);
+                } else {
+                    int i = 0;
+                    for (; i < positions.size(); i++) {
+                        Position position = positions.get(i);
+                        byte[] bytes = (position.toString() + '\n').getBytes(StandardCharsets.UTF_8);
+                        client.write(ByteBuffer.wrap(bytes));
+                    }
+                    client.write(ByteBuffer.allocateDirect('\n'));
+                    System.out.println("<-- wrote back the response (" + i + " results)");
+                }
+            }
         }
-        // System.out.println("result: " + positions);
-        for (int i = 0; i < positions.size(); i++) {
-            Position position = positions.get(i);
-            System.out.println(position);
+    }
+
+    private static void query(String searchWord) throws IOException {
+        UnixDomainSocketAddress address = UnixDomainSocketAddress.of(socketPath);
+        try (SocketChannel channel = SocketChannel.open(address)) {
+            String request = searchWord + "\n";
+
+            byte[] bytes = request.getBytes(StandardCharsets.UTF_8);
+            channel.write(ByteBuffer.wrap(bytes));
+
+            // Read response until empty line
+            ByteBuffer buffer = ByteBuffer.allocate(4096);
+            StringBuilder response = new StringBuilder();
+
+            while (channel.read(buffer) > 0) {
+                buffer.flip();
+                response.append(StandardCharsets.UTF_8.decode(buffer));
+                buffer.clear();
+
+                // Check for the end of response (empty line = "\n\n")
+                if (response.toString().endsWith("\n\n")) break;
+            }
+
+            // Print results (trim trailing empty line)
+            System.out.print(response.toString().stripTrailing());
         }
     }
 }
