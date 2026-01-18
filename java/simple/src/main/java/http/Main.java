a28c8dedcf8c693d636f1f114e2d49e64f5f56e9
Bartek Pacia <barpac02@gmail.com>
2026-01-18 21:57:13 +0100

    try with resourecs

--- java/simple/src/main/java/http/Main.java
+++ java/simple/src/main/java/http/Main.java
@@ -7,13 +7,14 @@ import java.net.http.HttpResponse;
 
 public class Main {
     static void main() {
-        HttpClient httpClient = HttpClient.newBuilder().build();
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.thecatapi.com/v1/images/search?limit=10"))
-                .build();
-        httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
+        try (HttpClient httpClient = HttpClient.newBuilder().build()) {
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.thecatapi.com/v1/images/search?limit=10"))
+                    .build();
+            httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(System.out::println)
+                    .join();
+        }
     }
 }
