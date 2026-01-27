f4e642a3dc52036a4baa68d2aa9fb4eaa04620a9
Bartek Pacia <barpac02@gmail.com>
2026-01-27 15:37:48 +0100

    duplicates!!!

---
+++ java/demo_cucumber_pl/src/main/java/duplicates/Bar.java
@@ -0,0 +1,73 @@
+package duplicates;
+
+class ThreeDuplicates {
+    void tripleOccurrence1() {
+        System.out.println("triple");
+        System.out.println("occurrence");
+        System.out.println("appears");
+        System.out.println("three");
+        System.out.println("times");
+        System.out.println("hello");
+        System.out.println("there");
+        System.out.println("fellow");
+        System.out.println("jetbrainer");
+    }
+
+    void tripleOccurrence2() {
+        System.out.println("triple");
+        System.out.println("occurrence");
+        System.out.println("appears");
+        System.out.println("three");
+        System.out.println("times");
+        System.out.println("hello");
+        System.out.println("there");
+        System.out.println("fellow");
+        System.out.println("jetbrainer");
+    }
+
+    void tripleOccurrence3() {
+        System.out.println("triple");
+        System.out.println("occurrence");
+        System.out.println("appears");
+        System.out.println("three");
+        System.out.println("times");
+        System.out.println("hello");
+        System.out.println("there");
+        System.out.println("fellow");
+        System.out.println("jetbrainer");
+    }
+}
+
+class TwoDuplicates {
+    void doubleOccurence1() {
+        System.out.println("HELLO");
+        System.out.println("THIS");
+        System.out.println("IS");
+        System.out.println("SOME");
+        System.out.println("EXAMPLE");
+        System.out.println("QUITE");
+        System.out.println("LONG");
+        System.out.println("REPEATED");
+        System.out.println("TEXT");
+        System.out.println("WHICH");
+        System.out.println("MAKES");
+        System.out.println("NO");
+        System.out.println("SENSE");
+    }
+
+    void doubleOccurence2() {
+        System.out.println("HELLO");
+        System.out.println("THIS");
+        System.out.println("IS");
+        System.out.println("SOME");
+        System.out.println("EXAMPLE");
+        System.out.println("QUITE");
+        System.out.println("LONG");
+        System.out.println("REPEATED");
+        System.out.println("TEXT");
+        System.out.println("WHICH");
+        System.out.println("MAKES");
+        System.out.println("NO");
+        System.out.println("SENSE");
+    }
+}
